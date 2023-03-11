40fa0b7e869acc7276c8684f069bfcb6086d0261
Yulia41 <yulya_smertina.03@mail.ru>
2023-03-11 14:06:40 +0500

    Исправленная таблица синусов

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         BufferedReader readName=new BufferedReader(new InputStreamReader(System.in));
         String name=readName.readLine();
         System.out.println("Hello, "+name);
+        readName.close();
         System.out.println();
         for(int i=0; i<=45;i+=5){
             double rad=(i*Math.PI)/180;
