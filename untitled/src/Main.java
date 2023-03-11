b5eb8a1e2bf3b087dc83d02499b8dc8cf92e9059
Yulia41 <yulya_smertina.03@mail.ru>
2023-03-11 14:03:42 +0500

    Таблица Синусов

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader readName=new BufferedReader(new InputStreamReader(System.in));
+        String name=readName.readLine();
+        System.out.println("Hello, "+name);
+        System.out.println();
+        for(int i=0; i<=45;i+=5){
+            double rad=(i*Math.PI)/180;
+            System.out.println("Синус "+i+ " градусов равен "+Math.sin(rad));
+        }
     }
 }
